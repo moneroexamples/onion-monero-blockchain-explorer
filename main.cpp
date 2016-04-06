e881cccc7832da1d3bba611a8b80e6d428e84d08
moneroexamples <moneroexamples@tuta.io>
2016-04-06 17:15:09 +0800

    blockchain height added

--- main.cpp
+++ main.cpp
@@ -11,12 +11,46 @@ using boost::filesystem::path;
 
 using namespace std;
 
+
+namespace epee {
+    unsigned int g_test_dbg_lock_sleep = 0;
+}
+
+
+
 int main() {
 
 
-    path blockchain_path {"/home/mwo/.bitmonero/lmdb"};
+    path blockchain_path {"/home/mwo/.bitmonero/lmdb/"};
 
     fmt::print("Blockchain path      : {}\n", blockchain_path);
+
+     // enable basic monero log output
+    xmreg::enable_monero_log();
+
+    // create instance of our MicroCore
+    xmreg::MicroCore mcore;
+
+    // initialize the core using the blockchain path
+    if (!mcore.init(blockchain_path.string()))
+    {
+        cerr << "Error accessing blockchain." << endl;
+        return 1;
+    }
+
+    // get the high level cryptonote::Blockchain object to interact
+    // with the blockchain lmdb database
+    cryptonote::Blockchain& core_storage = mcore.get_core();
+
+
+    // get the current blockchain height. Just to check
+    // if it reads ok.
+    uint64_t height = core_storage.get_current_blockchain_height() - 1;
+
+    fmt::print("\n\n"
+          "Top block height      : {:d}\n", height);
+
+
 
 
     std::string view{"{{#names}}Hi {{name}}!\n{{/names}}"};
