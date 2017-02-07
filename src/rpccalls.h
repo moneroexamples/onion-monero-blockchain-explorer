a0f85e68ebfc218626c252994a5a33e6b62f879e
moneroexamples <moneroexamples@tuta.io>
2017-02-07 00:56:17 +0000

    rpccalls.h updated to match current monero rpc changes

--- src/rpccalls.h
+++ src/rpccalls.h
@@ -23,6 +23,8 @@ namespace xmreg
         string deamon_url ;
         uint64_t timeout_time;
 
+        std::chrono::milliseconds timeout_time_ms;
+
         epee::net_utils::http::url_content url;
 
         epee::net_utils::http::http_simple_client m_http_client;
@@ -34,26 +36,29 @@ namespace xmreg
 
         rpccalls(string _deamon_url = "http:://127.0.0.1:18081",
                  uint64_t _timeout = 200000)
-        : deamon_url {_deamon_url}, timeout_time {_timeout}
+        : deamon_url {_deamon_url},
+          timeout_time {_timeout}
         {
             epee::net_utils::parse_url(deamon_url, url);
 
             port = std::to_string(url.port);
+
+            timeout_time_ms = std::chrono::milliseconds {timeout_time};
+
+            m_http_client.set_server(deamon_url);
         }
 
         bool
         connect_to_monero_deamon()
         {
-            std::lock_guard<std::mutex> guard(m_daemon_rpc_mutex);
+            //std::lock_guard<std::mutex> guard(m_daemon_rpc_mutex);
 
             if(m_http_client.is_connected())
             {
                 return true;
             }
 
-            return m_http_client.connect(url.host,
-                                         port,
-                                         timeout_time);
+            return m_http_client.connect(timeout_time_ms);
         }
 
         uint64_t
@@ -64,9 +69,15 @@ namespace xmreg
 
             std::lock_guard<std::mutex> guard(m_daemon_rpc_mutex);
 
-            bool r = epee::net_utils::invoke_http_json_remote_command2(
-                    deamon_url + "/getheight",
-                    req, res, m_http_client, timeout_time);
+            if (!connect_to_monero_deamon())
+            {
+                cerr << "get_current_height: not connected to deamon" << endl;
+                return false;
+            }
+
+            bool r = epee::net_utils::invoke_http_json(
+                    "/getheight",
+                    req, res, m_http_client, timeout_time_ms);
 
             if (!r)
             {
@@ -83,16 +94,23 @@ namespace xmreg
         }
 
         bool
-        get_mempool(vector<tx_info>& mempool_txs) {
+        get_mempool(vector<tx_info>& mempool_txs)
+        {
 
             COMMAND_RPC_GET_TRANSACTION_POOL::request  req;
             COMMAND_RPC_GET_TRANSACTION_POOL::response res;
 
             std::lock_guard<std::mutex> guard(m_daemon_rpc_mutex);
 
-            bool r = epee::net_utils::invoke_http_json_remote_command2(
-                    deamon_url + "/get_transaction_pool",
-                    req, res, m_http_client, timeout_time);
+            if (!connect_to_monero_deamon())
+            {
+                cerr << "get_mempool: not connected to deamon" << endl;
+                return false;
+            }
+
+            bool r = epee::net_utils::invoke_http_json(
+                    "/get_transaction_pool",
+                    req, res, m_http_client, timeout_time_ms);
 
             if (!r)
             {
@@ -122,10 +140,15 @@ namespace xmreg
 
             std::lock_guard<std::mutex> guard(m_daemon_rpc_mutex);
 
-            bool r = epee::net_utils::invoke_http_json_remote_command2(deamon_url
-                                                                       + "/sendrawtransaction",
-                                                                       req, res,
-                                                                       m_http_client, 200000);;
+            if (!connect_to_monero_deamon())
+            {
+                cerr << "commit_tx: not connected to deamon" << endl;
+                return false;
+            }
+
+            bool r = epee::net_utils::invoke_http_json(
+                    "/sendrawtransaction",
+                    req, res, m_http_client, timeout_time_ms);
 
             if (!r || res.status == "Failed")
             {
