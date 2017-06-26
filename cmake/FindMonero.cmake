749a0f28bd59615b291c73202f384049ec6357a4
moneroexamples <moneroexamples@tuta.io>
2017-06-26 10:10:17 +0800

    updated to current Monero development version

--- cmake/FindMonero.cmake
+++ cmake/FindMonero.cmake
@@ -30,7 +30,7 @@
 
 set(LIBS common;blocks;cryptonote_basic;cryptonote_core;
 		cryptonote_protocol;daemonizer;mnemonics;epee;lmdb;
-		blockchain_db;ringct;wallet)
+		blockchain_db;ringct;wallet;cncrypto)
 
 set(Xmr_INCLUDE_DIRS "${CPP_MONERO_DIR}")
 
@@ -61,12 +61,6 @@ endforeach()
 if (EXISTS ${MONERO_BUILD_DIR}/external/unbound/libunbound.a)
 	add_library(unbound STATIC IMPORTED)
 	set_property(TARGET unbound PROPERTY IMPORTED_LOCATION ${MONERO_BUILD_DIR}/external/unbound/libunbound.a)
-endif()
-
-if (EXISTS ${MONERO_BUILD_DIR}/src/crypto/libcrypto.a)
-	add_library(cryptoxmr STATIC IMPORTED)
-	set_property(TARGET cryptoxmr
-			PROPERTY IMPORTED_LOCATION ${MONERO_BUILD_DIR}/src/crypto/libcrypto.a)
 endif()
 
 if (EXISTS ${MONERO_BUILD_DIR}/external/easylogging++/libeasylogging.a)
