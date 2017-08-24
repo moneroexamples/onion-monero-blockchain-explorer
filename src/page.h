513b59004f9f89c58c6096ba795c640322cf5964
moneroexamples <moneroexamples@tuta.io>
2017-08-24 13:42:13 +0800

    Median block size renamed to Median block size limit

--- src/page.h
+++ src/page.h
@@ -727,7 +727,7 @@ namespace xmreg
 
             string block_size_limit = fmt::format("{:0.2f}",
                               static_cast<double>(
-                                      current_network_info.block_size_limit / 2) / 1024.0);
+                                      current_network_info.block_size_limit) / 2.0 / 1024.0);
 
             context["network_info"] = mstch::map {
                     {"difficulty"        , current_network_info.difficulty},
