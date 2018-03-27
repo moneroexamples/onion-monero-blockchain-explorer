2fdeea62250a1174bfaae97f86860c721f3ea95e
stoffu <stoffu@protonmail.ch>
2018-03-27 20:23:21 +0900

    build: add missing libmultisig

--- cmake/FindMonero.cmake
+++ cmake/FindMonero.cmake
@@ -28,7 +28,7 @@
 # (c) 2014-2016 cpp-ethereum contributors.
 #------------------------------------------------------------------------------
 
-set(LIBS common;blocks;cryptonote_basic;cryptonote_core;
+set(LIBS common;blocks;cryptonote_basic;cryptonote_core;multisig;
 		cryptonote_protocol;daemonizer;mnemonics;epee;lmdb;device;
 		blockchain_db;ringct;wallet;cncrypto;easylogging;version;checkpoints)
 
