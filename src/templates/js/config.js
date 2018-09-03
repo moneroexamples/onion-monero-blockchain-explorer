2d0324c0a29f5e617d56fce4277e7121231d54b0
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-09-04 00:17:15 +0300

    Thanks RYO

--- src/templates/js/config.js
+++ src/templates/js/config.js
@@ -1,13 +1,13 @@
 var config = {
     testnet: false, // this is adjusted page.h if needed. dont need to change manually
     stagenet: false, // this is adjusted page.h if needed. dont need to change manually
-    coinUnitPlaces: 12,
+    coinUnitPlaces: 9,
     txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
     txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-    coinSymbol: 'XMR',
-    openAliasPrefix: "xmr",
-    coinName: 'Monero',
-    coinUriPrefix: 'monero:',
+    coinSymbol: 'RYO',
+    openAliasPrefix: "ryo",
+    coinName: 'Ryo',
+    coinUriPrefix: 'ryo:',
     addressPrefix: 18,
     integratedAddressPrefix: 19,
     subAddressPrefix: 42,
@@ -20,11 +20,11 @@ var config = {
     feePerKB: new JSBigInt('2000000000'),//20^10 - for testnet its not used, as fee is dynamic.
     dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
     txChargeRatio: 0.5,
-    defaultMixin: 4, // minimum mixin for hardfork v5
+    defaultMixin: 12, // minimum mixin for hardfork v5
     txChargeAddress: '',
     idleTimeout: 30,
     idleWarningDuration: 20,
     maxBlockNumber: 500000000,
-    avgBlockTime: 120,
+    avgBlockTime: 240,
     debugMode: false
 };
