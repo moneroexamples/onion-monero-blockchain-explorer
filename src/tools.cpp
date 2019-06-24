627805e8a8d2245cf2d4904c0f2eba078d518083
stoffu <stoffu@protonmail.ch>
2019-06-24 14:07:45 +0900

    tools: pass kdf_rounds to generate_chacha_key

--- src/tools.cpp
+++ src/tools.cpp
@@ -1048,7 +1048,7 @@ decrypt(const std::string &ciphertext,
     }
 
     crypto::chacha_key key;
-    crypto::generate_chacha_key(&skey, sizeof(skey), key);
+    crypto::generate_chacha_key(&skey, sizeof(skey), key, 1);
 
     const crypto::chacha_iv &iv = *(const crypto::chacha_iv*)&ciphertext[0];
 
